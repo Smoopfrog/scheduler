1933921bea74a06336ca3779cdb170ab41d55289
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-08-09 16:05:02 -0700

    add test for DayListItem and fixed DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,34 @@
 import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
+
+const formatSpots = spots => {
+  if(!spots) {
+    return "no spots remaining"
+  }
+  
+  if(spots === 1) {
+    return "1 spot remaining"
+  }
+
+  return `${spots} spots remaining`
+}
 
 export default function DayListItem(props) {
+  const dayClass = classNames(
+    'day-list__item',
+    {'day-list__item--selected': props.selected}
+  );
+
+  const spotString = formatSpots(props.spots);
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li 
+      className={dayClass} 
+      onClick={() => props.setDay(props.name)}
+    >
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{spotString}</h3>
     </li>
   );
 }

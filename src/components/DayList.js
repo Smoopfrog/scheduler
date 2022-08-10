f633c079a5efa580dc649fcb68c10d7d63587010
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-08-10 14:46:00 -0700

    create a new array of interviews with individual props

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,12 +4,12 @@ import DayListItem from "./DayListItem";
 export default function DayList(props) {
   const schedule = props.days.map(dayObj => {
     return (
-      <DayListItem 
-        key={dayObj.id}
-        name={dayObj.name}
-        spots={dayObj.spots}
-        selected={dayObj.name === props.day}
-        setDay={props.setDay}
+      <DayListItem
+        key = {dayObj.id}
+        name = {dayObj.name}
+        spots = {dayObj.spots}
+        selected = {dayObj.name === props.day}
+        setDay = {props.setDay}
       />
     )
   })

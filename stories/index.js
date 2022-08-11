287e3f56adbf11c91b570b576e624bb549bf12e2
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-08-10 17:24:13 -0700

    add appointment views and components

--- stories/index.js
+++ stories/index.js
@@ -7,11 +7,15 @@ import InterviewerListItem from "components/InterviewerListItem";
 import Button from "components/Button";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
+import Show from "components/Appointment/Show";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 // CSS
 import "index.scss";
-import Header from "components/Appointment/Header";
-import Empty from "components/Appointment/Empty";
 
 storiesOf("Button", module)
   .addParameters({
@@ -153,5 +157,31 @@ storiesOf("Appointment", module)
   .add("Empty", () => (
     <Empty
       onAdd={action("onAdd")}
+    />
+  ))
+  .add("Show", () => (
+    <Show
+      student="Lydia Miller-Jones"
+      interviewer={interviewer}
+      onEdit={action("onEdit")}
+      onDelete={action("onDelete")}
+    />
+  ))
+  .add("Confirm", () => (
+    <Confirm 
+      message="Delete the appointment?"
+      onConfirm={action("onConfirm")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Status", () => (
+    <Status
+      message="Deleting"
+    />
+  ))
+  .add("Error", () => (
+    <Error
+      message="Could not delete appointment."
+      onClose={action("onClose")}
     />
   ))
